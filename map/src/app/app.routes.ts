caf2b4a5310e5e99c9b931bfb5a7b43674cf595e
Jim-LA <54000789+Jim-LA@users.noreply.github.com>
2024-01-23 18:54:40 +0100

    Init

--- map/src/app/app.routes.ts
+++ map/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { CrudComponent } from './crud/crud.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', title: 'Accueil', component: HomeComponent},
+    {path: 'map', title: 'Carte', component: MapComponent},
+    {path: 'crud', title: 'Villes', component: CrudComponent},
+    {path: '**', component: HomeComponent}
+    
+];
---
+++ map/src/app/crud/crud.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css'
+})
+export class CrudComponent {
+  towns = [ 
+    { id:1, ville: 'London', lat: 51.5074, lng: 0.1278 },
+    { id:2, ville: 'Paris', lat: 48.8566, lng: 2.3522 },
+    { id:3, ville: 'Madrid', lat: 40.4168, lng: -3.7038 },
+    { id:4, ville: 'Berlin', lat: 52.5200, lng: 13.4050 },
+    { id:5, ville: 'Rome', lat: 41.9028, lng: 12.4964 },];
+}
